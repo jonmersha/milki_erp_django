ca5cb65541b700596bdad0f4d229e59700f173fa
jonmersha@gmail.com <jonmersha@gmail.com>
2025-08-31 00:28:34 +0300

    kkkkk

--- milki/settings.py
+++ milki/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -127,7 +128,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'   # collected static files
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',   # your custom static folder
+]
 
 
 # Default primary key field type
