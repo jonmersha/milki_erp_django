4e32bae52b51e13feee54983d40eeba65be107e3
jonmersha@gmail.com <jonmersha@gmail.com>
2025-08-31 00:13:07 +0300

    pure

--- milki/settings.py
+++ milki/settings.py
@@ -6,7 +6,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG = True
+DEBUG = False
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
