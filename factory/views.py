84dbb6b5b971298d05467f98e342837a58f2ed14
jonmersha@gmail.com <jonmersha@gmail.com>
2025-08-31 01:30:43 +0300

    adminOptions

--- factory/views.py
+++ factory/views.py
@@ -16,30 +16,31 @@ from rest_framework import  filters, viewsets,permissions, serializers, decorato
 from rest_framework.exceptions import ValidationError
 
 
-from django.http import HttpResponse
-from django.contrib.auth.models import User
-
 
 from django.http import HttpResponse
 from django.views import View
-from django.contrib.auth.models import User
 
+from django.contrib.auth import get_user_model
+
+User = get_user_model()  # uses your custom core.User
 
 class CreateAdminUserView(View):
     def get(self, request, *args, **kwargs):
         token = request.GET.get("token")
 
-        # # simple security check
+        # # Simple security check
         # if token != "MY_SECRET_KEY":
         #     return HttpResponse("Unauthorized", status=401)
 
-        username = "admin"
-        email = "admin@gmail.com"
+        username = "milkiadmin"
+        email = "milkiadmin@besheger.com"
         password = "Yohannes@hira123321"
 
+        # Check if user already exists
         if User.objects.filter(username=username).exists():
             return HttpResponse("Admin user already exists.")
 
+        # Create superuser using your custom User model
         User.objects.create_superuser(username=username, email=email, password=password)
         return HttpResponse("Admin user created successfully.")
 
