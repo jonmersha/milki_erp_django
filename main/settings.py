539b0f7ecd554ba998b643e8e49e9efc429da4c5
jonmersha@gmail.com <jonmersha@gmail.com>
2026-01-01 05:16:44 +0300

    dbsetting

--- main/settings.py
+++ main/settings.py
@@ -102,8 +102,8 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'cafe6',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'milki7',
         'HOST': 'localhost',
         'USER': 'storeuser',
         'PASSWORD': 'Yohannes@123321'
